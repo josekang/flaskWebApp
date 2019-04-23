7f7287c9f0147361b84e1343259493a51ead0e53
josekang <35797936+josekang@users.noreply.github.com>
2019-04-23 12:15:55 +0300

    Update __init__.py

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -31,7 +31,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = "josekangethe2@gmail.com"
-app.config["MAIL_PASSWORD"] = "#39JoseKeliza"
+
 
 db = SQLAlchemy(app)
 Migrate(app, db)
